9523c7525d64be6b5b76d005c9ad52791e395aa0
azzamjafri <azzamjafri98@gmail.com>
2020-06-24 12:51:11 +0530

    Uncommit

--- lib/Registration/email_verification.dart
+++ lib/Registration/email_verification.dart
@@ -22,20 +22,23 @@ class _EmailVerificationState extends State<EmailVerification> {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Padding(padding: EdgeInsets.only(top: 30.0, bottom: 10.0), child: logo(),),
+        Padding(
+          padding: EdgeInsets.only(top: 30.0, bottom: 10.0),
+          child: logo(),
+        ),
         verificationCard(),
         // detailsCard(),
       ],
     );
   }
 
-
   verificationCard() {
     return Container(
       height: 250.0,
-      width: MediaQuery.of(context).size.width ,
+      width: MediaQuery.of(context).size.width,
       child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
         margin: EdgeInsets.all(20.0),
         child: Padding(
           padding: const EdgeInsets.only(top: 20.0, right: 30.0, left: 30.0),
@@ -43,25 +46,25 @@ class _EmailVerificationState extends State<EmailVerification> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Email Verification', style: TextStyle(fontSize: 16.5, fontWeight: FontWeight.bold)),
-              Padding(padding: EdgeInsets.all(5.0),),
-              Text('We have send an email with subject line : (Dello MarketPlcae) - Account Verification to your registered email address'),
-              
+              Text('Email Verification',
+                  style:
+                      TextStyle(fontSize: 16.5, fontWeight: FontWeight.bold)),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+              ),
+              Text(
+                  'We have send an email with subject line : (Dello MarketPlcae) - Account Verification to your registered email address'),
             ],
           ),
         ),
       ),
     );
-
   }
 
   logo() {
     return Center(
       child: Container(
-        height: 120.0,
-        width: 120.0,
-        child: Image.asset('assets/logo.png')
-      ),
+          height: 120.0, width: 120.0, child: Image.asset('assets/logo.png')),
     );
   }
 }
